b4ab683df320f63eb79f296bd2d60dbe357ee120
UkJang <mag0225@naver.com>
2024-11-08 10:11:20 +0900

    Add Calculator Test

---
+++ src/main/java/com/jake/jenkinspipeline/Calculator.java
@@ -0,0 +1,12 @@
+package com.jake.jenkinspipeline;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+    public Integer sum(Integer a, Integer b){
+        return a+b;
+    }
+}
+
